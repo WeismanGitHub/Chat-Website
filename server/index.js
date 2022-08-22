1575c773a7a4fec33448858ad9840dc93814bde4
Weisman <weisman123@outlook.com>
2022-08-21 20:23:27 -0700

    changed mongo uri from local to production

--- server/index.js
+++ server/index.js
@@ -74,7 +74,7 @@ app.use(errorHandler)
 // start
 try {
     const port = process.env.PORT || 5000
-    mongoose.connect(process.env.LOCAL_MONGO_URI, { autoIndex: true })
+    mongoose.connect(process.env.MONGO_URI, { autoIndex: true })
     console.log('Connected to database...')
     server.listen(port, console.log(`Server is starting on ${port}...`));
 } catch (err) {
